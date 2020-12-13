90f3075b565b5bacc49a5226e6264a63835e28b2
Victor <v.bueno@aluno.ifsp.edu.br>
2020-12-13 16:50:28 -0300

    Initial Commit

---
+++ MinhasEncomendas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MinhasEncomendas
+//
+//  Created by Victor on 13/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
