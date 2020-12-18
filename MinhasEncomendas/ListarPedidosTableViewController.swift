87f05cc256e5fc24732f9b92e6aa1e825505a93b
Victor <v.bueno@aluno.ifsp.edu.br>
2020-12-18 20:09:18 -0300

    Adicionar delete

--- MinhasEncomendas/ListarPedidosTableViewController.swift
+++ MinhasEncomendas/ListarPedidosTableViewController.swift
@@ -84,17 +84,23 @@ class ListarPedidosTableViewController: UITableViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let pedido = self.pedidos[indexPath.row]
+            self.context.delete(pedido)
+            
+            self.pedidos.remove(at: indexPath.row)
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            
+            do {
+                try self.context.save()
+            } catch let erro {
+                print("Erro ao remover item: \(erro.localizedDescription)")
+            }
+        }
     }
-    */
 
     /*
     // Override to support rearranging the table view.
